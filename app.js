d9cd78a00b0dc8a8dc53d4a830e527e54e567d0a
Float07 <39651883+Float07@users.noreply.github.com>
2023-02-21 18:52:18 -0300

    Implement hamburger menu

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-document.querySelector('#hamburger-menu');
+const hamburgerButton = document.getElementById('hamburger-menu');
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileMenuItems = document.getElementsByClassName("mobile-link-container");
+console.log(mobileMenuItems);
+
+hamburgerButton.addEventListener('click', () => {
+  hamburgerButton.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+});
+
+for (let index = 0; index < mobileMenuItems.length; index++) {
+  const element = mobileMenuItems[index];
+  element.addEventListener('click', () => {
+    hamburgerButton.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+  });
+}
+
+mobileMenuItems[0].addEventListener('click', () => {
+  document.getElementById("us").scrollIntoView({behavior: 'smooth'});
+});
+
+mobileMenuItems[1].addEventListener('click', () => {
+  document.getElementById("services").scrollIntoView({behavior: 'smooth'});
+});
+
+mobileMenuItems[2].addEventListener('click', () => {
+  document.getElementById("social").scrollIntoView({behavior: 'smooth'});
+});
+
+mobileMenuItems[3].addEventListener('click', () => {
+  document.getElementById("location").scrollIntoView({behavior: 'smooth'});
+});
